03c41669215bb4181e840e0999dd50efc0b1d662
YZY_PRO <976809588@qq.com>
2017-07-04 14:48:30 +0800

    init

--- PickFiveDemo/main.m
+++ PickFiveDemo/main.m
@@ -8,10 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+NSInteger pickNum(NSDictionary *existingDic){
+    
+    NSInteger arcNum = arc4random() % 11 + 1;
+    
+    if ([existingDic objectForKey: [NSString stringWithFormat: @"%ld", arcNum]]) {
+        return pickNum(existingDic);
+    }
+    
+    return arcNum;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableDictionary *resultDict = [NSMutableDictionary dictionary];
+        
+        for (NSInteger i = 0; i < 5; i++) {
+            
+            NSInteger arcNum = pickNum(resultDict);
+            [resultDict setObject: @" " forKey: [NSString stringWithFormat: @"%ld",arcNum]];
+            
+            NSLog(@"%ld ",arcNum);
+        }
     }
     return 0;
 }
+
