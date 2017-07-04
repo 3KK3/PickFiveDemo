e3c0dbc83ab1f7d99ff504d6908476af2377f359
YZY_PRO <976809588@qq.com>
2017-07-04 14:02:22 +0800

    Initial Commit

---
+++ PickFiveDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PickFiveDemo
+//
+//  Created by YZY on 2017/7/4.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
